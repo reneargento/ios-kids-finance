6ff56cb2459fcbeaf9ad39e00143599a609e87b1
Rene Argento <reneargento@Renes-MacBook-Pro.local>
2015-03-25 10:26:35 -0300

    Initial commit

---
+++ KidsFinance/KidsFinance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KidsFinance
+//
+//  Created by Rene Argento on 3/25/15.
+//  Copyright (c) 2015 Umbrella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

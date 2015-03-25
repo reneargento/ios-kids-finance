c27cb3fd259b53c1e915b1ca7805b0daccaf37b1
Rafael  Letro <rafaelletro@MacBook-Pro-de-Rafael.local>
2015-03-25 10:37:06 -0300

    teste push

--- KidsFinance/KidsFinance/ViewController.m
+++ KidsFinance/KidsFinance/ViewController.m
@@ -15,7 +15,7 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    [super viewDidLoad]; 
     // Do any additional setup after loading the view, typically from a nib.
 }
 

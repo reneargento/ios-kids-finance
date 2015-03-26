9fa831049ff02088049535ee5b8f4520034c5777
Carolina Bonturi <carolbonturi@Carolinas-MacBook-Pro.local>
2015-03-26 11:06:53 -0300

    merge commit

--- KidsFinance/KidsFinance/ViewController.h
+++ KidsFinance/KidsFinance/ViewController.h
@@ -7,11 +7,15 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <sqlite3.h>
 
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *currentMoneyLabel;
 @property (weak, nonatomic) IBOutlet UILabel *savingsLabel;
+@property (strong, nonatomic) NSString *databasePath; // create a ponter to database in APP
+@property (nonatomic)sqlite3 *DB; 
+
 
 @property (weak, nonatomic) IBOutlet UIImageView *addMoneyButton;
 @property (weak, nonatomic) IBOutlet UIImageView *substractMoneyButton;

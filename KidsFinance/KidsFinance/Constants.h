5e7c581b5bc0b1f5e464a2c864a6dcf8be943be9
Rene <rene.argento@gmail.com>
2015-04-01 10:42:28 -0300

    Added keychain wrapper

---
+++ KidsFinance/KidsFinance/Constants.h
@@ -0,0 +1,16 @@
+//
+//  Constants.h
+//  KidsFinance
+//
+//  Created by Rene Argento on 4/1/15.
+//  Copyright (c) 2015 Umbrella. All rights reserved.
+//
+
+#ifndef KidsFinance_Constants_h
+#define KidsFinance_Constants_h
+
+#define keychainIdentifier @"KIDS_FINANCE";
+#define currentMoneyKey @"CURRENT_MONEY";
+#define savingsKey @"SAVINGS";
+
+#endif
---
+++ KidsFinance/KidsFinance/Utils.m
@@ -0,0 +1,29 @@
+//
+//  Utils.m
+//  KidsFinance
+//
+//  Created by Rene Argento on 4/1/15.
+//  Copyright (c) 2015 Umbrella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Utils.h"
+#import "KeychainItemWrapper.h"
+#import "Constants.h"
+
+@interface Utils ()
+
+@end
+
+@implementation Utils
+
+-(void)saveValueInKeychain: (NSString*)key withValue:(NSString *)value {
+   // KeychainItemWrapper *keychain = [[KeychainItemWrapper alloc] initWithIdentifier:keychainIdentifier accessGroup:nil];
+}
+
+-(NSString *)getValueFromKeychain:(NSString *)key {
+    return nil;
+}
+
+@end
+
